3587b11a7e784d75526664db2006008c9ebf9228
pratham4544 <coolboy6397@gmail.com>
2024-05-28 10:47:17 +0530

    add css in streamlit and reduce the ai models

--- app.py
+++ app.py
@@ -15,7 +15,42 @@ os.environ['LANGCHAIN_API_KEY'] = os.getenv('LANGCHAIN_API_KEY')
 # db = get_vector_store(text_chunks=text_chunks)
 embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 
+def inject_css():
+    custom_css = """
+    <style>
+    :root {
+        --background-light: #ffffff;
+        --background-dark: #1e1e1e;
+        --text-light: #000000;
+        --text-dark: #ffffff;
+        --highlight-light: #f1f1f1;
+        --highlight-dark: #333333;
+    }
 
+    @media (prefers-color-scheme: light) {
+        .reply-box, .shlok-box, .meaning-box, .example-box {
+            background-color: var(--background-light);
+            color: var(--text-light);
+            border: 1px solid var(--highlight-light);
+        }
+    }
+
+    @media (prefers-color-scheme: dark) {
+        .reply-box, .shlok-box, .meaning-box, .example-box {
+            background-color: var(--background-dark);
+            color: var(--text-dark);
+            border: 1px solid var(--highlight-dark);
+        }
+    }
+
+    .box {
+        padding: 10px;
+        border-radius: 10px;
+        margin: 10px 0;
+    }
+    </style>
+    """
+    st.markdown(custom_css, unsafe_allow_html=True)
 
 def user_input(user_question):
     print(user_question)
@@ -31,16 +66,27 @@ def user_input(user_question):
     
     # Run the chain with the retrieved context and the user's question
     response = chain.invoke({"context": docs, "question": user_question})
+    
+    parts = response.strip().split("\n\n")
+    reply = parts[0]
+    shlok = parts[1].replace("Shlok:  ", "")
+    meaning = parts[2].replace("Meaning: ", "")
+    example = parts[3].replace("Example: ", "")
+    
 
     # Create a container to display the response
     with st.container():
-        st.write("Reply: ", response)
+        st.markdown(f'<div class="box reply-box">{reply}</div>', unsafe_allow_html=True)
+        st.markdown(f'<div class="box shlok-box">{shlok}</div>', unsafe_allow_html=True)
+        st.markdown(f'<div class="box meaning-box">{meaning}</div>', unsafe_allow_html=True)
+        st.markdown(f'<div class="box example-box">{example}</div>', unsafe_allow_html=True)
+  
         
         st.write("Generationg img for you Arjun...")
         
-        img_prompt = text_to_img_prompt(response=response)
+        # img_prompt = text_to_img_prompt(response=response)
         
-        image = img_generator(img_prompt=img_prompt)
+        image = img_generator(img_prompt=example)
         
         st.image(image)
 
@@ -53,7 +99,8 @@ def user_input(user_question):
     
     
 def main():
-    st.set_page_config("Chat Lord Krishna")
+    inject_css()
+    # st.set_page_config("Chat Lord Krishna")
     st.header("Chat with Bhagwan Shri Krishna 🌟")
     st.subheader("Seeking Guidance for Life's Questions")
 
